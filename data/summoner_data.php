2377cd4439ccf119a5cee4d26cfb3b71e3562dbc
Patrick <patrick.morgenegg@gmail.com>
2015-04-27 21:47:26 +0200

    Added summonerid requests

--- data/summoner_data.php
+++ data/summoner_data.php
@@ -1,16 +1,25 @@
 <?php
 
-$apikey = "";
+# Includes and requires
+require_once('data/summoner_data.php');
+
+#definitions
+$apikey = "apikey";
 $region = "euw";
-$summoner_name = "whitetrill";
+#$summoner_name = "whitetrill";
+$summoner_name= $_POST['summonername'];
+$region= $_POST['region'];
 
 
 function summoner_id ($region, $summoner_name, $apikey){
-	$url = "https://'.$region.'.api.pvp.net/api/lol/'.$region.'/v1.4/summoner/by-name/'.$summoner_name.'?api_key='.$apikey.'";
-	$content = file_get_contents($url);
-	$result = json_decode($content, true);
-	echo $result;
-}
-summoner_id($region, $summoner_name, $apikey);
+	$url = @file_get_contents('https://'.$region.'.api.pvp.net/api/lol/'.$region.'/v1.4/summoner/by-name/'.$summoner_name.'?api_key='.$apikey.'');
+	$result = json_decode($url);
+	
+	foreach ($result as $list){
+        	$summonerid = $list->id;
+        	break;
+	}
 
+	echo $summonerid;
+}
 ?>
--- index.php
+++ index.php
@@ -6,7 +6,30 @@
   </head>
   <body>
 	<?php
-		echo "League of Legends wrapper"
+		require_once('data/summoner_data.php');
+	?>
+		<br>
+	<h1>League of Legends wrapper</h1>
+	<form method="post" action="">
+                <select name="region" id="region">
+                        <option value="euw">EU West</option>
+                        <option value="br">Brasil</option>
+                        <option value="eune">EU North</option>
+                        <option value="kr">Korea</option>
+                        <option value="las">Latin South</option>
+                        <option value="lan">Latin North</option>
+                        <option value="na">North America</option>
+                        <option value="oce">Oceania</option>
+                        <option value="tr">Turkey</option>
+                        <option value="ru">Russia</option>
+                </select>
+                <input type="text" name="summonername">
+                <input type="submit">
+        </form>
+
+	<?php
+		echo "Summoner ID:" . $summonerid;
+		summoner_id($region, $summoner_name, $apikey);
 	?>
   </body>
 </html>
